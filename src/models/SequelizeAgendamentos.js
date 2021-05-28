57b2a0478b47d544e29562b83b824b2a94e84586
Caroline Aguiar <carolineclarissa19@gmail.com>
2021-05-28 12:27:46 -0300

    Criação métodos criar e buscar

--- src/models/SequelizeAgendamentos.js
+++ src/models/SequelizeAgendamentos.js
@@ -3,11 +3,58 @@ const TabelaAgendamento = require('./TabelaAgendamento');
 module.exports = {
     async listar() {
         try {
-            return await TabelaAgendamento.findAll({
-                raw = true
+            result = await TabelaAgendamento.findAll({
+                raw: true
             });
+            return result
         } catch(error) {
             throw error;
+        };
+    },
+
+    async buscarPorPK(id) {
+        try {
+            result = await TabelaAgendamento.findByPk(id);
+            return result
+        } catch (error) {
+            throw error
+        }
+    },
+
+    async adicionar(agendamento) {
+        try {
+            result = await TabelaAgendamento.create(agendamento);
+            return result
+        } catch(error) {
+            throw error
+        }
+    },
+
+    async atualizar(id, dados) {
+        try {
+            result = await TabelaAgendamento.update(dados,
+                {
+                    where: {
+                        id:id
+                    }
+                }
+            );
+            return result
+        } catch (error) {
+            throw error
+        }
+    },
+
+    async remover(id) {
+        try {
+            result = await TabelaAgendamento.destry({
+                where: {
+                    id:id
+                }
+            });
+            return result
+        } catch (error) {
+            throw error
         }
     }
-}
+};
